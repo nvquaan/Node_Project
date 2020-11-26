0a8cb8b56e39c283c86f1ad20bf7ebe03da1b242
nvquaan <quan.ng99@gmail.com>
2020-11-26 22:13:10 +0700

    css a tags

---
+++ src/app/models/Login.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const login = new Schema({
+    email: { type: String, default: ''},
+    password: { type: String, default: ''},
+
+    loginAt: { type: Date, default: Date.now},
+    logoutAt: { type: Date, default: Date.now},
+    action: { type: String, default: 'System'},
+    
+}, { collection: 'login' })
