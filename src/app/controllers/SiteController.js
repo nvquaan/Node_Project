dbf198b4d001be644d43c17302e2eebc0622b8c5
Nguyen Van Quan <kbsa2499@gmail.com>
2021-08-08 17:02:30 +0700

    them bao cao

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -8,8 +8,8 @@ class SiteController {
     async getAllCourses(req, res, next) {
         try {
             let courses = await Course.find({});
-                courses = courses.map((course) => course.toObject());
-                res.render("home", { courses });
+            courses = courses.map((course) => course.toObject());
+            res.render("home", { courses });
         }
         catch (err) {
             next(err);
@@ -54,11 +54,40 @@ class SiteController {
                 name: r.name,
                 rateAvg: r.rateAvg,
             }
+        });
+
+        //Báo cáo khoá học theo lượt Mua
+        //get dsách user => lặp mỗi user lấy ra mảng courses cho vào mảng chính. Lặp qua mảng đó để count số lượng mỗi khoá học được bán
+        let user = await User.find({}).populate(
+            {
+                path: "courses",
+                populate: { path: "course", select: ["name"] },
+            }
+        );
+        let boughtCourses = []; //mảng lưu danh danh sách các khoá học đc mua (có trùng)
+        user.forEach(u => {
+            boughtCourses.push(...(u.courses.map(c => c.course)));
         })
-        res.render("report", {categories: JSON.stringify(categories), courses: JSON.stringify(courses), rates: JSON.stringify(rates), rateOfCourses: JSON.stringify(rateOfCourses)});
+
+        let objCount = {}; // obj count số lượng mua của các khoá học ở trên
+        boughtCourses.forEach(c => {
+            objCount[c._id] = (objCount[c._id] || 0) + 1
+        })
+        let countedBought = [];
+        for(let prop in objCount) {
+            let name = boughtCourses.find(c => c._id == prop)['name'];
+            countedBought.push({name, count: objCount[prop]});
+        }
+        res.render("report", {
+            countedBought: JSON.stringify(countedBought),
+            categories: JSON.stringify(categories),
+            courses: JSON.stringify(courses),
+            rates: JSON.stringify(rates),
+            rateOfCourses: JSON.stringify(rateOfCourses),
+        });
     }
 
-    logout (req, res, next) {
+    logout(req, res, next) {
         try {
             req.session.destroy();
             res.redirect('/');
